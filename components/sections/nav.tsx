03312ed55f71b63629319ae2ce16d44bb7707289
josh-brms <bermasjoshua12345@gmail.com>
2026-09-09 00:57:33 +0800

    fix: restore original JoshStudio / JB navbar branding

--- components/sections/nav.tsx
+++ components/sections/nav.tsx
@@ -71,7 +71,9 @@ export function Nav() {
               draggable={false}
               className="h-8 w-8 object-contain light:invert"
             />
-            <span className="whitespace-nowrap">Joshua Bermas</span>
+                <span className="whitespace-nowrap">
+                  JB<span className="text-accent2">.</span>
+                </span>
           </button>
 
           <ul className="hidden items-center gap-7 md:flex">
@@ -147,7 +149,7 @@ export function Nav() {
                   draggable={false}
                   className="h-8 w-8 object-contain light:invert"
                 />
-                <span className="whitespace-nowrap">Joshua Bermas</span>
+            <span className="whitespace-nowrap">JoshStudio</span>
               </span>
               <button
                 onClick={() => setMenuOpen(false)}
